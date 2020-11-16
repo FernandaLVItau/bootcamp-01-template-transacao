b4425e63cd3ae438bf2eace42b93f3244b8bbf97
Fernanda Vedovello <fe.vedovello@gmail.com>
2020-11-16 17:34:40 -0300

    first commit

---
+++ src/main/java/br/com/zup/cartaotransacao/CartaoTransacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.cartaotransacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartaoTransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartaoTransacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/cartaotransacao/CartaoTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.cartaotransacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartaoTransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
